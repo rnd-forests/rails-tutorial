8fe99d84576a4e90286d937380cdd50c1dfa541b
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2015-02-27 18:06:34 +0700

    Adjust sidebar.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,18 +7,11 @@ module UsersHelper
         image_tag(gravatar_url, alt: user.name)
     end
 
-    def top_creators
-        User.joins(:articles).select('users.*, count(user_id) AS article_count')
-            .group('users.id')
-            .order('article_count DESC')
-            .limit(10)
-    end
-
-    def top_followers
-        User.select('users.*, COUNT(*) AS follower_count')
-            .joins('INNER JOIN relationships ON relationships.followed_id = users.id')
-            .group('relationships.followed_id')
-            .order('follower_count DESC')
-            .limit(5)
-    end
+    # def top_followers
+    #     User.select('users.*, COUNT(*) AS follower_count')
+    #         .joins('INNER JOIN relationships ON relationships.followed_id = users.id')
+    #         .group('relationships.followed_id')
+    #         .order('follower_count DESC')
+    #         .limit(5)
+    # end
 end
