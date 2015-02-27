c25370423ae6847eba0c91166d1a4bde8e08ec90
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2015-02-27 23:16:46 +0700

    Add favicon.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,12 +6,4 @@ module UsersHelper
         gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
         image_tag(gravatar_url, alt: user.name)
     end
-
-    # def top_followers
-    #     User.select('users.*, COUNT(*) AS follower_count')
-    #         .joins('INNER JOIN relationships ON relationships.followed_id = users.id')
-    #         .group('relationships.followed_id')
-    #         .order('follower_count DESC')
-    #         .limit(5)
-    # end
 end
