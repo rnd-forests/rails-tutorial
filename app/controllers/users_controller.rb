3b54b9793f277da127ebbc23af269966b6fcbd3d
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2015-03-09 17:23:12 +0700

    Change user index view.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     before_action :admin_user, only: :destroy
 
     def index
-        @users = User.order('name').paginate(page: params[:page], per_page: 24)
+        @users = User.order('name').paginate(page: params[:page], per_page: 30)
     end
 
     def show
